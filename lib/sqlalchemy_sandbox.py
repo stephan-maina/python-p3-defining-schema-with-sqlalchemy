c98bf8bc22e28daf86a7f050a4a121a10bc5164a
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-06 01:44:59 +0300

    Completed Lab

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
-
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class Student(Base):
-    pass
+    """
+    Represents a student.
+    """
+    __tablename__ = 'students'
+    
+    id = Column(Integer(), primary_key=True, doc="Unique student identifier")
+    name = Column(String(), doc="Student's name")
+
+    def __repr__(self):
+        return f"Student(id={self.id}, name='{self.name}')"
+
+def create_database():
+    engine = create_engine('sqlite:///students.db')
+    Base.metadata.create_all(engine)
 
 if __name__ == '__main__':
-    pass
+    create_database()
